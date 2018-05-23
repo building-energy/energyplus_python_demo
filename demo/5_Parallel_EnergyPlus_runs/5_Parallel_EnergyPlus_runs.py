7a764f53336628c7d26fa053942f813022b0a1aa
Steven Firth <s.k.firth@lboro.ac.uk>
2018-05-23 18:11:26 +0100

    updates

--- demo/5_Parallel_EnergyPlus_runs/5_Parallel_EnergyPlus_runs.py
+++ demo/5_Parallel_EnergyPlus_runs/5_Parallel_EnergyPlus_runs.py
@@ -3,13 +3,38 @@
 from multiprocessing import Pool
 import os
 import datetime
+import shutil
+
+
+def run_expand_objects(idf_fp):
+    """Runs the ExpandObjects program
+    
+    Places the exapnded idf in the same folder as the 'idf_fp' file,
+        with the name os.path.basename('df_fp')+'exp.idf'
+    
+    Arguments:
+        idf_fp (str): the absolute filepath of the idf file - including the extension
+    
+    """
+    new_fp=idf_fp[:-4]+'exp.idf'
+    shutil.copyfile(idf_fp,r'C:\EnergyPlusV8-9-0\in.idf')
+    now=datetime.datetime.now()
+    os.system(r'C:\EnergyPlusV8-9-0\ExpandObjects')
+    exp_fp=r'C:\EnergyPlusV8-9-0\expanded.idf'
+    if os.path.isfile(exp_fp):
+        unix_ts=os.path.getmtime(exp_fp)
+        t=datetime.datetime.fromtimestamp(unix_ts)
+        if t>now:
+            shutil.copyfile(exp_fp,new_fp)
+            return new_fp
+    shutil.copyfile(idf_fp,new_fp)
+    return new_fp
+    
 
 def run_energyplus(epexe_fp,
                    out_fp,
                    idf_fp,
                    epw_fp,
-                   output_prefix='eplus',
-                   display=True
                    ):
         """Runs the EnergyPlus software
         
@@ -18,7 +43,6 @@ def run_energyplus(epexe_fp,
             - out_fp (str): the absolute filepath of the output folder
             - idf_fp (str): the absolute filepath of the idf file - including the extension
             - epw_fp (str): the absolute filepath of the epw file - including the extension
-            - display(boolean):
         
         """
         
@@ -26,21 +50,20 @@ def run_energyplus(epexe_fp,
         if not os.path.isdir(out_fp):
             os.mkdir(out_fp)
             
-        #DELETES THE 'eplusout.expidf' FILE IN 'out_fp' IF IT'S PRESENT
-        #    this is needed to force the recreation of this file...
-        expidf_fp=os.path.join(out_fp,output_prefix+'out.expidf')
-        if os.path.isfile(expidf_fp):
-            os.remove(expidf_fp) 
-            
+        #MANUALLY RUNS THE ExpandObjects PROGRAM
+        idf_fp_new=run_expand_objects(idf_fp)
+        
+        #SETS THE OUTPUT PREFIX
+        output_prefix=os.path.splitext(os.path.basename(idf_fp))[0]
+        
         #RUN ENERGYPLUS VIA SUBPROCESS.POPEN
         l=[epexe_fp,
-           '-x',
            '-r',
            '-c',
            '-d',out_fp,
            '-p',output_prefix,
            '-w',epw_fp,
-           idf_fp]
+           idf_fp_new]
         
         st=' '.join(l)
         
@@ -50,41 +73,39 @@ def run_energyplus(epexe_fp,
         
     
 def run_idf(idf_fp):
-    "Runs EnergyPlus based on the idf filepath provided"
+    """Runs EnergyPlus based on the idf filepath provided
+    
+    Places the resultsin the same folder as the idf file
+    
+    Arguments:
+        idf_fp (str): the absolute filepath of the idf file - including the extension
+        
+    
+    """
     epexe_fp=r'C:\EnergyPlusV8-9-0\EnergyPlus'
-    out_fp=os.path.abspath('sim')
-    idf_fp=os.path.join(out_fp,idf_fp)
+    out_fp=os.path.dirname(idf_fp)
     epw_fp=r'C:\EnergyPlusV8-9-0\WeatherData\USA_CA_San.Francisco.Intl.AP.724940_TMY3.epw'
-    output_prefix=os.path.splitext(os.path.basename(idf_fp))[0]
     run_energyplus(epexe_fp,
                    out_fp,
                    idf_fp,
-                   epw_fp,
-                   output_prefix
+                   epw_fp
                   )
-
 
 if __name__ == '__main__': # ... needed for Pool...
     
-    l=['1ZoneUncontrolled_0.idf',
-       '1ZoneUncontrolled_90.idf',
-       '1ZoneUncontrolled_180.idf',
-       '1ZoneUncontrolled_270.idf']
-
+    l=[os.path.join('sim','1ZoneUncontrolled_0.idf'),
+       os.path.join('sim','1ZoneUncontrolled_90.idf'),
+       os.path.join('sim','1ZoneUncontrolled_180.idf'),
+       os.path.join('sim','1ZoneUncontrolled_270.idf')]
+    
     now=datetime.datetime.now()
     print('START_TIME',now.strftime('%Y-%m-%d %H:%M:%S'))
     
-    with Pool(2) as p: #the number here is the number of threads
+    with Pool(4) as p: #the number here is the number of threads
         p.map(run_idf,l)
-        
-#SOMETIMES AN IDF DOESN'T RUN
-#   It's not clear why this happens
-#   Different idfs don't run at different times
-#   This can be picked up by viewing the modified times of the .csv files (see below)
-#   One solution would be to identify the failures and rerun them...
-        
+                
     for i in l:
-        fp=os.path.join('sim',i[:-4]+'out.csv')
+        fp=i[:-4]+'out.csv'
         unix_ts=os.path.getmtime(fp)
         t=datetime.datetime.fromtimestamp(unix_ts)
-        print(i, t.strftime('%Y-%m-%d %H:%M:%S'), 'SUCCESS' if t>now else 'FAIL')
+        print(os.path.basename(i), t.strftime('%Y-%m-%d %H:%M:%S'), 'SUCCESS' if t>now else 'FAIL')
